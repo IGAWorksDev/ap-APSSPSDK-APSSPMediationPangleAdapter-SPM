3970ebbc6877e1d47e675204319819d4ecb10818
HwanghoSong <odin.song@adpopcorn.com>
2026-09-08 17:57:26 +0900

    709010100.0.0(7.9.1.1.0 Adapter)배포

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/IGAWorksDev/ap-APSSPSDK-SPM.git", from: "3.4.0"),
-        .package(url: "https://github.com/bytedance/AdsGlobalPackage.git", exact: "7.9.0-release.8"),
+        .package(url: "https://github.com/bytedance/AdsGlobalPackage.git", exact: "7.9.1-release.1"),
     ],
     targets: [
         .target(name: "APSSPMediationPangle",
