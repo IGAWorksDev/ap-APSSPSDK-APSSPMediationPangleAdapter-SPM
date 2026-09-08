f62aab0132ad1582a6742645973519d0e6e634d8
HwanghoSong <odin.song@adpopcorn.com>
2026-09-08 17:59:45 +0900

    801010200.0.0(8.1.1.2.0 Adapter)배포

--- Sources/MediationPangle/PangleAdapterFactory.swift
+++ Sources/MediationPangle/PangleAdapterFactory.swift
@@ -8,7 +8,7 @@ public final class PangleAdapterFactory: NSObject, APSSPAdapterFactory {
     public static var networkID: Int { 18 }
 
     public static var sdkVersion: String? { PangleInitializationAdapter().sdkVersion }
-    public static var adapterVersion: String? { "8.1.1.1.0" }
+    public static var adapterVersion: String? { "8.1.1.2.0" }
 
     public static func makeInitializationAdapter() -> AnyObject? {
         return PangleInitializationAdapter()
